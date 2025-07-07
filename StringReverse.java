56686ca7477488926af9e38ac08236fbb67e7724
Donti Arun Kumar <donthidibbu4@gmail.com>
2025-07-07 20:07:33 +0530

    Update StringReverse.java

--- StringReverse.java
+++ StringReverse.java
@@ -3,7 +3,7 @@ package strings;
 public class StringReverse {
 
 	public static void main(String[] args) {
-		String name = "Dibbu";
+		String name = "Arun";
 		String rev="";
 		for(int i=name.length()-1; i>=0; i--) {
 			rev += name.charAt(i);
