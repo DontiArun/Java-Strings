ff3e9c7b03564fc873ff9375ae5d310212226bbd
Donti Arun Kumar <donthidibbu4@gmail.com>
2025-07-13 21:16:04 +0530

    Update StringMemoryExample.java

--- StringMemoryExample.java
+++ StringMemoryExample.java
@@ -6,9 +6,12 @@ public class StringMemoryExample{
         String s2="hello";
         String s3=new String("hello");
         
-        System.out.println(s1==s2);
-        System.out.println(s1.equals(s2));
-        System.out.println(s1==s3);
-        System.out.println(s1.equals(s3));
+        System.out.println(s1==s2);   //it checks whether they both s1 & s2 pointing to same memory or not.
+        
+        System.out.println(s1.equals(s2));   //it checks whether content in s1 and s2 equal or not.
+        
+        System.out.println(s1==s3);    //it checks whether they both s1 & s3 pointing to same memory or not.
+        
+        System.out.println(s1.equals(s3));   //it checks whether content in s1 and s3 equal or not.
     }
 }
